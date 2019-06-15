82957410e4888e7ce79f959bcb871e27a20ea1cc
shabira <skhan>
2019-06-16 02:02:59 +0530

    i added a java file

---
+++ xyz.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class xyz
+{  
+	public static void main(String[] args) {  
+	Scanner input = new Scanner(System.in);
+	System.out.print("enter the number : ");
+
+	int number = input.nextInt();
+	switch(number){  
+    case 10: System.out.println("10");
+	   break;  
+    case 20: System.out.println("20");
+	   break;  
+    case 30: System.out.println("30");
+	   break;  
+    default:System.out.println("Not in 10, 20 or 30");  
+    }  
+}  
+}  
+
