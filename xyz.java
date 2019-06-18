25ad5a61715a5b55c9563445be090817d0e37e3f
mohsinkhan994 <41528429+mohsinkhan994@users.noreply.github.com>
2019-06-18 09:01:39 +0530

    updated xyz

--- xyz.java
+++ xyz.java
@@ -2,20 +2,10 @@ import java.util.Scanner;
 public class xyz
 {  
 	public static void main(String[] args) {  
-	Scanner input = new Scanner(System.in);
-	System.out.print("enter the number : ");
+	
+	System.out.print("This is the first change");
 
-	int number = input.nextInt();
-	switch(number){  
-    case 10: System.out.println("10");
-	   break;  
-    case 20: System.out.println("20");
-	   break;  
-    case 30: System.out.println("30");
-	   break;  
-    default:System.out.println("Not in 10, 20 or 30");  
-    }
-    System.out.println("i didnot changes");  
+	 
 }  
 }  
 
